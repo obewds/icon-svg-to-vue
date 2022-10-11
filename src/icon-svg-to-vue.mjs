f4f355937bac6bc46aa59526d7ea0f1952a0bab9
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-10-11 00:33:57 -0400

    refactored cli script and cleaned up code now in helpers

--- src/icon-svg-to-vue.mjs
+++ src/icon-svg-to-vue.mjs
@@ -1,16 +1,9 @@
 #!/usr/bin/env node
 // ./src/icon-svg-to-vue.mjs
 
-import beautify from 'beautify'
-import chalk from 'chalk'
-import fs from 'fs-extra'
-
-import cleanAndBeautify from './helpers/cleanAndBeautify.mjs'
-import generateVueFile from './helpers/generateVueFile.mjs'
 import iconSvgToVueFile from './helpers/iconSvgToVueFile.mjs'
-import processConsoleLogs from './helpers/processConsoleLogs.mjs'
-import scriptSuccessConsoleLogs from './helpers/scriptSuccessConsoleLogs.mjs'
-import wrongArgsConsoleErrors from './helpers/wrongArgsConsoleErrors.mjs'
+import showProcessInfoConsoleLogs from './helpers/showProcessInfoConsoleLogs.mjs'
+import showWrongArgsConsoleError from './helpers/showWrongArgsConsoleError.mjs'
 
 
 const cwd = process.env.INIT_CWD
@@ -18,45 +11,12 @@ const args = process.argv
 
 
 if (args.length !== 4) {
-
-    wrongArgsConsoleErrors()
+    showWrongArgsConsoleError()
     process.exit()
-
 }
 
 
-processConsoleLogs(cwd, args[2], args[3])
+showProcessInfoConsoleLogs(cwd, args[2], args[3])
 
-/*
-async function loadFileAsync (userSvgFilePath, userOutputFilePath) {
-
-    const originFile = cwd + '/' + userSvgFilePath
-    const outputFile = cwd + '/' + userOutputFilePath
-
-    await fs.pathExists(originFile).then( async (exists) => {
-
-        // file exists!
-
-        // read the file data asynchronously
-        fs.readFile(originFile, 'utf8', function(err, data) {
-
-            if (err) { throw err }
-            
-            fs.outputFileSync(
-                outputFile,
-                generateVueFile( cleanAndBeautify(data), userOutputFilePath),
-                { flag: 'w+' }
-            )
-
-            scriptSuccessConsoleLogs()
-
-        })
-
-    }).catch( (error) => { throw error } )
-
-}
-*/
-
-//await loadFileAsync(args[2], args[3])
 
 await iconSvgToVueFile(cwd, args[2], args[3])
