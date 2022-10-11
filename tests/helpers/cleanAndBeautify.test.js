2bf04b483a2cacb2e77605eafe0a4ed80ec6052c
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-10-11 02:29:13 -0400

    removing console log from test

--- tests/helpers/cleanAndBeautify.test.js
+++ tests/helpers/cleanAndBeautify.test.js
@@ -15,8 +15,6 @@ test('Text that cleanAndBeautify returns an expected value', async () => {
     const svg = `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg"><!--! Legal disclaimer comment example --><path d="M20,230 Q40,205 50,230 T90,230"></path></svg>`
 
     const vueFile = cleanAndBeautify(svg, 'some/test/SvgVueName.vue')
-
-    console.log(vueFile)
     
     expect(vueFile).toBeTruthy()
     expect(vueFile).toContain('<!-- Legal')
