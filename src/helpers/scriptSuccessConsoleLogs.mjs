c723f2c13ede51fe84b4850c812b99ab963c3824
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-10-11 00:32:26 -0400

    adding scriptSuccessConsoleLogs cli helper

---
+++ src/helpers/scriptSuccessConsoleLogs.mjs
@@ -0,0 +1,12 @@
+// ./src/helpers/scriptSuccessConsoleLogs.mjs
+
+import chalk from 'chalk'
+
+export default function () {
+
+    console.log(chalk.greenBright('*****************************'))
+    console.log(chalk.greenBright('* SVG CONVERSION COMPLETED! *'))
+    console.log(chalk.greenBright('*****************************'))
+    console.log(' ')
+
+}
