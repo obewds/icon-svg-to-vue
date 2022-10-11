2202523afe9798d2e58e682b17dfca033631162a
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-10-11 00:29:38 -0400

    adding iconSvgToVueFile.test.js file

---
+++ tests/helpers/iconSvgToVueFile.test.js
@@ -0,0 +1,10 @@
+// ./tests/helpers/iconSvgToVueFile.test.js
+
+import iconSvgToVueFile from '../../src/helpers/iconSvgToVueFile.mjs'
+
+
+test('Text that importing iconSvgToVueFile returns a truthy value', async () => {
+
+    expect(iconSvgToVueFile).toBeTruthy()
+
+})
