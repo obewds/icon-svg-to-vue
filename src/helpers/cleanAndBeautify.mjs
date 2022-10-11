577c4953959ac62d7b4774645cd25d180225a925
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-10-11 02:25:45 -0400

    added comments to clean and beautify script to help record the why's of the mutations

--- src/helpers/cleanAndBeautify.mjs
+++ src/helpers/cleanAndBeautify.mjs
@@ -4,7 +4,10 @@ import beautify from 'beautify'
 
 export default function (fileContents) {
 
-    const cleaned = fileContents.replace(/!--!/g, '!--')// .replace(/></g, '>\n<')
+    const cleaned = fileContents.replace(/!--!/g, '!--') // remove FontAwesome's comment keeping exclamation point which causes weird typescript issues
+                                .replace(' class="w-6 h-6"', '') // remove heroicons default svg class attr and value
+                                .replace(' class="w-5 h-5"', '') // remove heroicons default mini svg class attr and value
+                                //.replace(/></g, '>\n<')
 
     const beautified = beautify(cleaned, {format: 'html'})
 
